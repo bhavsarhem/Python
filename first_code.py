36cdb2d8037e9273270392fc0670ee9165fa7434
Hem_Bhavsar <101062997+bhavsarhem@users.noreply.github.com>
2022-12-16 22:09:52 +0530

    Create first_code.py
    This is "HELLO WORLD!" program in python.

---
+++ first_code.py
@@ -0,0 +1,16 @@
+# just print the 'HELLO WORD!
+print("HELLO WORLD!")
+
+# or
+print('HELLO WORLD!')
+
+# or
+a1 = "HELLO"
+a2 = "WORLD!"
+print(a1 +" "+ a2)
+
+# or 
+# also you can use function
+def sentence():
+    print('HELLO WORLD!')
+sentence()
